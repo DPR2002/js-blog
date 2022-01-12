3f7b655af86281a343433802be92abdc384f4d47
DPR2002 <danielreinert185@gmail.com>
2022-01-12 20:53:19 +0100

    add article

--- js/script.js
+++ js/script.js
@@ -1,31 +1,38 @@
-document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  });
-
   const titleClickHandler = function(event){
     const clickedElement = this;
-    console.log('Link was clicked!');
   
     /* [DONE] remove class 'active' from all article links  */
   
     const activeLinks = document.querySelectorAll('.titles a.active');
   
-    for(let activeLink of activeLinks){
-      activeLink.classList.remove('active');
+    for(let link of activeLinks){
+      link.classList.remove('active');
     }
   
     /* [IN PROGRESS] add class 'active' to the clicked link */
   
-    console.log('clickedElement:', clickedElement);
+
+    clickedElement.classList.add('active');
   
     /* [DONE] remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('article.active');
+
+    for(let article of activeArticles){
+      article.classList.remove('active');
+    }
   
     /* get 'href' attribute from the clicked link */
+
+    const articleId = clickedElement.getAttribute("href");
   
     /* find the correct article using the selector (value of 'href' attribute) */
+
+    const articleToDisplay = document.querySelector(articleId);
   
     /* add class 'active' to the correct article */
+
+    articleToDisplay.classList.add('active')
   }
   
   const links = document.querySelectorAll('.titles a');
